6184458fa833123f2e340ea0f8a8837127a22d57
Muzammil <muzammilrashid3108@gmail.com>
2025-11-20 04:38:15 +0500

    SU I

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:devs/src/routes/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'core/theme/theme.dart';
 
 class DevsApp extends StatelessWidget {
@@ -7,12 +8,19 @@ class DevsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      routerConfig: appRoutes,
+    return ScreenUtilInit(
+        designSize: const Size(390, 844), // Your UI design size (Figma size)
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            themeMode: ThemeMode.system,
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            routerConfig: appRoutes,
+          );
+        }
     );
   }
 }
